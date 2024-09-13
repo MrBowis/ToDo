6055f679013a476218165d213a1eb3da36436150
MrBowis <02alejo20@gmail.com>
2024-09-12 21:47:56 -0500

    Fix login settings.py && Change port for nginx

--- login/login/settings.py
+++ login/login/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-s614_6-@s0h$0h=)%dqpj($7=d_0zx43tjp#e6aplxgq8kcptw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -117,7 +117,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_ROOT = '/static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
