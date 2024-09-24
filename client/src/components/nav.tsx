7fee3447fcca900de5c00f45c43442d8576c0649
MrBowis <02alejo20@gmail.com>
2024-09-24 18:58:22 -0500

    Fix navbar - Client

--- client/src/components/nav.tsx
+++ client/src/components/nav.tsx
@@ -10,93 +10,64 @@ import {
   Link,
   Button,
 } from "@nextui-org/react";
-import { AcmeLogo } from "./AcmeLogo.jsx";
 
 export default function App() {
   const [isMenuOpen, setIsMenuOpen] = React.useState(false);
 
   const menuItems = [
-    "Profile",
-    "Dashboard",
-    "Activity",
-    "Analytics",
-    "System",
-    "Deployments",
-    "My Settings",
-    "Team Settings",
-    "Help & Feedback",
-    "Log Out",
+    { name: "Task", href: "task" },
+    { name: "Group", href: "group" },
+    { name: "Integrations", href: "integrations" },
   ];
 
   return (
     <>
-      <Navbar
-        isBordered
-        isMenuOpen={isMenuOpen}
-        onMenuOpenChange={setIsMenuOpen}
-      >
-        <NavbarContent className="sm:hidden" justify="start">
+      <Navbar onMenuOpenChange={setIsMenuOpen}>
+        <NavbarContent>
           <NavbarMenuToggle
             aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+            className="sm:hidden"
           />
-        </NavbarContent>
-
-        <NavbarContent className="sm:hidden pr-3" justify="center">
           <NavbarBrand>
-            <AcmeLogo />
-            <p className="font-bold text-inherit">ACME Hola</p>
+            <p className="font-bold text-inherit">To Do</p>
           </NavbarBrand>
         </NavbarContent>
 
         <NavbarContent className="hidden sm:flex gap-4" justify="center">
-          <NavbarBrand>
-            <AcmeLogo />
-            <p className="font-bold text-inherit">ACME</p>
-          </NavbarBrand>
-          <NavbarItem>
-            <Link color="foreground" href="#">
-              Features
-            </Link>
-          </NavbarItem>
-          <NavbarItem isActive>
-            <Link href="#" aria-current="page">
-              Customers
-            </Link>
-          </NavbarItem>
-          <NavbarItem>
-            <Link color="foreground" href="#">
-              Integrations
-            </Link>
-          </NavbarItem>
+          {menuItems.map((item, index) => (
+            <NavbarItem key={`${index}-${item}`}>
+              <Link color="foreground" href={item.href}>
+                {item.name}
+              </Link>
+            </NavbarItem>
+          ))}
         </NavbarContent>
-
         <NavbarContent justify="end">
           <NavbarItem className="hidden lg:flex">
-            <Link href="#">Login</Link>
+            <Link href="login">Login</Link>
           </NavbarItem>
           <NavbarItem>
-            <Button as={Link} color="warning" href="#" variant="flat">
+            <Button as={Link} color="primary" href="register" variant="flat">
               Sign Up
             </Button>
           </NavbarItem>
         </NavbarContent>
-
         <NavbarMenu>
           {menuItems.map((item, index) => (
             <NavbarMenuItem key={`${item}-${index}`}>
               <Link
-                className="w-full"
                 color={
                   index === 2
-                    ? "warning"
+                    ? "primary"
                     : index === menuItems.length - 1
                     ? "danger"
                     : "foreground"
                 }
-                href="#"
+                className="w-full"
+                href={item.href}
                 size="lg"
               >
-                {item}
+                {item.name}
               </Link>
             </NavbarMenuItem>
           ))}
